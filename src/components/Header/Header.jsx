c1e69a7b8046fe69c27a90893e748c02e6da2db2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-09-23 02:47:43 +0600

    header component doneand it is reandered from app.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import profile from "../../assets/profile.png";
+
+const Header = () => {
+  return (
+    <>
+      <div className="md:px-12">
+        <div className="flex justify-between ">
+          <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+          <img className="w-12" src={profile} alt="" />
+        </div>
+        <hr className="border-1 my-2" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
